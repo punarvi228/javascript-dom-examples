a0c252eea905a607dcfb0b8e75341523190de615
punarvi <punarvi.punnu@gmail.com>
2021-12-19 12:04:09 +0530

    Added html page and js page

---
+++ js/script.js
@@ -0,0 +1,54 @@
+/*function add() {
+    var a = 1;
+    var b = 2;
+    var c = 3;
+    var d = 4;
+    var e = 5;
+
+    var result = (a + b + c + d + e);
+    alert(result);
+};
+add();*/
+// (function() {
+//     var a = 1;
+//     var b = 2;
+//     var result = (a * b);
+//     console.log(result);
+// })();
+// var animals = ["cat", "lion"];
+// animals.push("rat");
+
+// animals.pop();
+
+// animals.unshift("monkey");
+
+// animals.shift();
+
+// animals.splice(1, 0, "pig");
+// console.log(animals);
+
+
+// var names = ["bachu", "bachu1"];
+
+// names.splice(1, 1, "bachu0");
+
+// names[0].length = "bachuNew";
+
+// console.log(names[1].length);
+
+// var fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// console.log(names);
+
+// delete fruits[0];
+
+// console.log(fruits);
+
+//var fruits = ["Banana", "Orange", "Apple", "Mango"];
+//fruits.splice(0, 1);
+
+//console.log(fruits);
+/*for (i = 0; i <= 10; i++) {
+    var result = i * 3;
+    console.log("3" + "*" + i + "=" + result);
+}*/
